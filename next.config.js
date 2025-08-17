146f2420a5f5dc8d0c975209b8a6315be8379c70
Arun Sudhakar <arunsudhakar456@gmail.com>
2025-08-17 22:41:34 +0100

    Fix: add basepath and assetprefix for GitHub pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',               // Required for static export
-  images: { unoptimized: true },  // GitHub Pages doesn’t support Next.js image optimization
-  basePath: '/Arun-portfolio',    // 👈 must match your repo name exactly
-  assetPrefix: '/Arun-portfolio/', 
-}
+  reactStrictMode: true,
+  output: "export",  // static export
+  basePath: "/Arun-portfolio",
+  assetPrefix: "/Arun-portfolio/",
+};
 
 module.exports = nextConfig;
